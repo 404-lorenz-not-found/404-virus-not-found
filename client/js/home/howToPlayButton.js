48c3f46cd438f2779dd250d88c3c385254236cc6
404-lorenz-not-found <66021959+404-lorenz-not-found@users.noreply.github.com>
2020-05-29 18:01:45 +0200

    Created front page
    # Created index.html content (front page)
    &
    # Added css/
    &
    # Added body.css, header.css, main.css and footer.css
    &
    # Created front page design in body.css, header.css, main.css and footer.css
    &
    # Added js/
    &
    # Added js/home/
    &
    # Added playButton.js, howToPlayButton.js, wikiButton.js and settingsButton.js
    &
    # Created button functions in playButton.js, howToPlayButton.js, wikiButton.js and settingsButton.js (playButtons and settingsButtons functions do nothing)

---
+++ client/js/home/howToPlayButton.js
@@ -0,0 +1,3 @@
+document.getElementById("howToPlayButton").onclick = function() {
+	window.open("https://github.com/404-lorenz-not-found/404-virus-not-found/wiki/How-to-play", "_blank");
+}
---
+++ client/js/home/wikiButton.js
@@ -0,0 +1,3 @@
+document.getElementById("wikiButton").onclick = function() {
+	window.open("https://github.com/404-lorenz-not-found/404-virus-not-found/wiki", "_blank");
+}
